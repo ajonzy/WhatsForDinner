efb772b507ca24ac23e5c2f943aaf96d3d35abb9
Alex Jones <ajonzy@gmail.com>
2022-08-19 19:09:17 -0600

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { Switch, Route } from 'react-router';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <Switch>
+          
+        </Switch>
+      </div>
+    );
+  }
+}
