5e2833499ed91f666db32bbecba837047c30d1fe
Alex Jones <ajonzy@gmail.com>
2022-08-22 12:45:02 -0600

    Added homepage

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -1,9 +1,31 @@
-import React, { useContext } from 'react'
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCalendarDays, faBurger, faScroll, faUserGroup } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home(props) {
     return (
         <div className='page-wrapper home-page-wrapper'>
-            Home
+            <div className="links-wrapper">
+                <Link to="/mealplans"><div className="link-box">
+                    <FontAwesomeIcon icon={faCalendarDays} />
+                    Mealplans
+                </div></Link>
+                <Link to="/meals"><div className="link-box">
+                    <FontAwesomeIcon icon={faBurger} />
+                    Meals
+                </div></Link>
+                <Link to="/shoppinglists"><div className="link-box">
+                    <FontAwesomeIcon icon={faScroll} />
+                    Shopping Lists
+                </div></Link>
+                <Link to="/friends"><div className="link-box">
+                    <FontAwesomeIcon icon={faUserGroup} />
+                    Friends
+                </div></Link>
+            </div>
+
+            <button>Create Mealplan</button>
         </div>
     )
 }
